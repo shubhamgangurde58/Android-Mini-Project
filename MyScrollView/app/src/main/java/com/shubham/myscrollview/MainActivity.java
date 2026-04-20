b7d1026684dbbf9b047d493c33bbacf314778ebd
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2026-04-20 21:50:16 +0530

    [ADDED]: shubham This code for My scroll View Demo Program in andoid

---
+++ MyScrollView/app/src/main/java/com/shubham/myscrollview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.shubham.myscrollview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
---
+++ MyScrollView/app/src/main/java/com/shubham/myscrollview/MyScrollViewActivity.java
@@ -0,0 +1,14 @@
+package com.shubham.myscrollview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyScrollViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myscrollview_layout);
+    }
+}
